87b151c39735c428bb26743956f293eb194e12d4
oramata <matabogeoratile@gmail.com>
2023-02-25 13:39:48 -0800

    Hexadecimal task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - main function is found here
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	for (i = 'a'; i < 'g'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
