802f3704ca642eeea4e4d76769fadb1bc08ea039
oramata <matabogeoratile@gmail.com>
2023-02-26 10:36:12 -0800

    Sign task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - function that  prints the sign of a number
+ *
+ * @n: function parameter number to check
+ *
+ * Return: 1 if n s greater than 0, 0 if n is 0, -1 if n is less than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
