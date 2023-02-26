04abb5f2b3694d2356c65bc559aafa0dc23424f9
oramata <matabogeoratile@gmail.com>
2023-02-26 04:39:42 -0800

    10 x alphabet task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - code found below will print 10 times the alphabet
+ *
+ * Return: always 0;
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+	}
+}
