8ec2e467763febf8fe21f05a78abc1c977719a81
oramata <matabogeoratile@gmail.com>
2023-02-27 17:00:09 -0800

    Don't hate the hacker, hate the code task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,24 +1,45 @@
+#include "main.h"
+#include <limits.h>
+
 /**
- * _atoi - Converts a string to an integer
- * @s: Pointer to the string to be converted
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: The integer value of the string
+ * Return: integer value of the string, or 0 if no number is found
  */
 int _atoi(char *s)
 {
-	int sign = 1, num = 0, i = 0;
+    int sign = 1;
+    int result = 0;
+    int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-			num = num * 10 + (s[i] - '0');
-		else if (num > 0)
-			break;
-
-		i++;
-	}
-
-	return (num * sign);
+    while (s[i] != '\0')
+    {
+        if (s[i] == '-')
+        {
+            sign = -1;
+        }
+        else if (s[i] >= '0' && s[i] <= '9')
+        {
+            int digit = s[i] - '0';
+            if (sign == 1 && (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)))
+            {
+                return INT_MAX;
+            }
+            else if (sign == -1 && (result > -(INT_MIN / 10) || (result == -(INT_MIN / 10) && digit > -(INT_MIN % 10))))
+            {
+                return INT_MIN;
+            }
+            else
+            {
+                result = result * 10 + digit;
+            }
+        }
+        else if (result > 0)
+        {
+            break;
+        }
+        i++;
+    }
+    return sign * result;
 }
