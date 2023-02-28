e037061a45fbd223abedd454b87607845d2c1145
oramata <matabogeoratile@gmail.com>
2023-02-27 16:00:14 -0800

    Half the lies they tell about me aren't true task

--- 0x05-pointers_arrays_strings/6-main.c
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - prints a string of every other character
  *
- * Return: Always 0.
+ * Description: calls put2 function to print every other charcter of a string,
+ * starting with the first character, followed by a new line.
+ *
+ * Return: Always 0
  */
+
 int main(void)
 {
-	char *str;
+	char *str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
 
-	str = "0123456789";
 	puts2(str);
 	return (0);
 }
