b9f5806d79fd2c2ccc695933ed8441fd0bc402c2
oramata <matabogeoratile@gmail.com>
2023-03-07 04:09:27 -0800

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,45 +1,46 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
  *
- * Return: integer value of the string, or 0 if no number is found
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int i = 0;
+	int i, d, n, len, f, digit;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-        {
-            sign = -1;
-        }
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            int digit = s[i] - '0';
-            if (sign == 1 && (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)))
-            {
-                return INT_MAX;
-            }
-            else if (sign == -1 && (result > -(INT_MIN / 10) || (result == -(INT_MIN / 10) && digit > -(INT_MIN % 10))))
-            {
-                return INT_MIN;
-            }
-            else
-            {
-                result = result * 10 + digit;
-            }
-        }
-        else if (result > 0)
-        {
-            break;
-        }
-        i++;
-    }
-    return sign * result;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
