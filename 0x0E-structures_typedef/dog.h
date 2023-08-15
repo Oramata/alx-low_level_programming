38a10217426adb5912da101bb287c9f3dd518c9b
Oramata <matabogeoratile@gmail.com>
2023-08-15 12:26:30 -0700

    dog.h task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents a dog's basic information.
+ * @name: Name of the dog (string).
+ * @age: Age of the dog (float).
+ * @owner: Name of the owner (string).
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
