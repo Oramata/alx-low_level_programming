8dc03c5f70bd88ccef529ce51ea24e613d299992
Oramata <matabogeoratile@gmail.com>
2023-09-07 00:29:33 -0700

    header file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX  standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of the letters read and print.
+ *
+ * Return: The actual number of letters read and printed.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t read_count, write_count;
+	char *buffer;
+
+	if (filename == NULL|)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (filename = NULL)
+		return (0);
+
+	fd = open (filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	 buffer = malloc(sizeof(char) * letters);
+	 if (buffer == NULL)
+		 return (0);
+	 {
+		 close (fd);
+		 return (0);
+	 }
+
+	 read_count = read(fd, buffer, letters);
+	 if (read_count == -1)
+	 {
+		 free(buffer);
+		 close(fd);
+		 return(0);
+	 }
+
+	 wirte_count = write(SDTDOUT_FILENO, buffer, read_count);
+
+	 if ?(write_count == -1 || wite_count != read_count)
+	 {
+		 free(buffer);
+		 close(fd);
+		 return (0);
+	 }
+
+	 free(buffer);
+	 close(fd);
+
+	 return (write_count);
+}
