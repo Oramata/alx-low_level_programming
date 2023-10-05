532a5fda50812f7983ee2ccd203b417cdff69ded
Oramata <matabogeoratile@gmail.com>
2023-10-05 09:05:35 -0700

    0-read_textfile.c tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,9 +4,10 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX  standard output.
+ * read_textfile - Reads a text file and,
+ * prints it to the POSIX standard output.
  * @filename: The name of the file to read.
- * @letters: The number of the letters read and print.
+ * @letters: The number of letters to read and print.
  *
  * Return: The actual number of letters read and printed.
  */
@@ -16,44 +17,38 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t read_count, write_count;
 	char *buffer;
 
-	if (filename == NULL|)
+	if (filename == NULL)
 		return (0);
 
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
-	if (filename = NULL)
-		return (0);
-
-	fd = open (filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buffer);
 		return (0);
+	}
 
-	 buffer = malloc(sizeof(char) * letters);
-	 if (buffer == NULL)
-		 return (0);
-	 {
-		 close (fd);
-		 return (0);
-	 }
+	read_count = read(fd, buffer, letters);
+	if (read_count == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-	 read_count = read(fd, buffer, letters);
-	 if (read_count == -1)
-	 {
-		 free(buffer);
-		 close(fd);
-		 return(0);
-	 }
+	write_count = write(STDOUT_FILENO, buffer, read_count);
 
-	 wirte_count = write(SDTDOUT_FILENO, buffer, read_count);
+	if (write_count == -1 || write_count != read_count)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-	 if ?(write_count == -1 || wite_count != read_count)
-	 {
-		 free(buffer);
-		 close(fd);
-		 return (0);
-	 }
+	free(buffer);
+	close(fd);
 
-	 free(buffer);
-	 close(fd);
-
-	 return (write_count);
+	return (write_count);
 }
